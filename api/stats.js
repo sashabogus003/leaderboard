778e7f92446692c11c2d7d5314af3ccc3395d01d
sashabogus003 <sashabogus003@gmail.com>
2025-09-19 23:14:21 +0300

    Update stats.js

--- api/stats.js
+++ api/stats.js
@@ -1,1 +1,14 @@
+export default async function handler(req, res) {
+  const { startTime, endTime } = req.query;
+
+  const url = `https://affiliate.shuffle.com/stats/${process.env.SHUFFLE_KEY}?startTime=${startTime}&endTime=${endTime}`;
+
+  try {
+    const r = await fetch(url);
+    const data = await r.json();
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ error: "Ошибка запроса" });
+  }
+}
 
